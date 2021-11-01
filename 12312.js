614096162f6491f84a6e5569f929e8d62d422ec4
ImAfif <afifadelayoub@gmail.com>
2021-11-01 21:41:44 +0000

    test lol

---
+++ 12312.js
@@ -0,0 +1,8 @@
+let money = 50 //how much money i have
+let product = 55 //how much teh product costs
+
+if (money >= product){
+  console.log("buy the item")
+} else {
+  console.log("you broke")
+}
